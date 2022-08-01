ed4066df84706681ebb6d23df16c0b4c696c91ff
Catse <45233053+catsethecat@users.noreply.github.com>
2022-08-01 12:54:07 +0300

    v1.1.0 changes

--- Plugin.cs
+++ Plugin.cs
@@ -10,6 +10,9 @@ using HarmonyLib;
 using TMPro;
 using System.Runtime.CompilerServices;
 using IPA.Config.Stores;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 [assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
 namespace SquatCounter
@@ -18,6 +21,8 @@ namespace SquatCounter
     {
         public static Config Instance { get; set; }
         public virtual long Timestamp { get; set; } = 0;
+        public virtual float StandingThreshold { get; set; } = 0.2f;
+        public virtual float SquatThreshold { get; set; } = 0.0f;
     }
 
     [Plugin(RuntimeOptions.SingleStartInit)]
@@ -28,6 +33,7 @@ namespace SquatCounter
 
         float wallHeight = 1.1f;
         int squatCount = 0;
+        WebSocketServer wsserv;
 
         [Init]
         public Plugin(IPALogger logger, IPA.Config.Config config)
@@ -35,6 +41,7 @@ namespace SquatCounter
             Instance = this;
             Log = logger;
             Config.Instance = config.Generated<Config>();
+            wsserv = new WebSocketServer();
         }
 
         [OnStart]
@@ -87,12 +94,13 @@ namespace SquatCounter
                         inputDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.devicePosition, out Vector3 pos);
                         inputDevices[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceRotation, out Quaternion ang);
                         float headY = pos.y + (ang * Vector3.forward * -0.1f).y;
-                        if (headY > wallHeight+0.1f)
+                        if (headY > (wallHeight+Config.Instance.StandingThreshold))
                             squatting = false;
-                        if (headY < (wallHeight-0.1f) && !squatting)
+                        if (headY < (wallHeight+Config.Instance.SquatThreshold) && !squatting)
                         {
                             squatting = true;
                             squatCount++;
+                            wsserv.BroadcastMessage("" + squatCount);
                             File.WriteAllText("UserData\\SquatCount.txt", "" + squatCount);
                         }
                         Thread.Sleep(11);
@@ -103,5 +111,89 @@ namespace SquatCounter
         }
 
 
+    }
+
+    public class WebSocketServer
+    {
+        TcpClient[] clients = new TcpClient[8];
+        int clientCount = 0;
+        public WebSocketServer()
+        {
+            new Thread(new ThreadStart(AcceptClientsThread)).Start();
+        }
+        private void AcceptClientsThread()
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 2350);
+            server.Start();
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                new Thread(() => ClientThread(client)).Start();
+            }
+        }
+        public void BroadcastMessage(string payload)
+        {
+            Byte[] msg = new byte[2 + payload.Length];
+            msg[0] = 0x81;
+            msg[1] = (byte)payload.Length;
+            Encoding.UTF8.GetBytes(payload).CopyTo(msg, 2);
+            int i = 0;
+            while (i < clientCount)
+            {
+                try
+                {
+                    clients[i].GetStream().Write(msg, 0, msg.Length);
+                    i++;
+                }
+                catch (Exception e)
+                {
+                    clientCount--;
+                    clients[i] = clients[clientCount];
+                }
+            }
+        }
+        private void ClientThread(TcpClient client)
+        {
+            if (clientCount == clients.Length)
+                return;
+            clients[clientCount] = client;
+            clientCount++;
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                while (true)
+                {
+                    while (!stream.DataAvailable && client.Connected) ;
+                    Byte[] bytes = new Byte[client.Available];
+                    stream.Read(bytes, 0, bytes.Length);
+                    String data = Encoding.UTF8.GetString(bytes);
+                    if (data.StartsWith("GET /squatcount"))
+                    {
+                        int i1 = data.ToLower().IndexOf("sec-websocket-key") + 19;
+                        int i2 = data.IndexOf("\r", i1);
+                        string key = data.Substring(i1, i2 - i1);
+                        Byte[] response = Encoding.UTF8.GetBytes(
+                            "HTTP/1.1 101 Switching Protocols\r\n" +
+                            "Connection: Upgrade\r\n" +
+                            "Upgrade: websocket\r\n" +
+                            "Sec-WebSocket-Accept: " + Convert.ToBase64String(
+                                 System.Security.Cryptography.SHA1.Create().ComputeHash(
+                                    Encoding.UTF8.GetBytes(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"))) + "\r\n" +
+                            "\r\n"
+                        );
+                        stream.Write(response, 0, response.Length);
+                    }
+                    else
+                    {
+                        if (bytes[0] == 0x89)
+                        {
+                            bytes[0] = 0x8A;
+                            stream.Write(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+            }
+            catch (Exception e) { }
+        }
     }
 }
